7299ee655dc643394bc0d9ebb387e14e9f482838
Bonita Scott <bonitalscott@yahoo.com>
2021-01-29 23:53:48 -0500

    updated token and .env app and usercontroller

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+require('dotenv').config();
 const express = require('express');
 const app = express();
 const sequelize = require('./db');
@@ -16,3 +17,10 @@ app.use('/user', user);
 app.listen(3000, function () {
   console.log('App is listening on port 3000');
 });
+
+// require('dotenv').config(); MUST be the First Line in app.js and allows us to make the secret
+// user credentials available to our entire application. Also, allows us to hide secret user credentials
+// from being uploaded to GitHub, to keep them from going Public
+//
+//sequelize.sync({force: true}) - drops table before create if model has changed. Only run ONCE, then
+//comment out. Not the best idea for PROD code, Drop or Truncate table in the DB (Postgres, or other)
--- controllers/usercontroller.js
+++ controllers/usercontroller.js
@@ -2,18 +2,18 @@ const router = require('express').Router();
 const User = require('../db').import('../models/user');
 const jwt = require('jsonwebtoken');
 
-// USER SIGNUP
-router.post('/create', function (req, res) {
+// USER REGISTER
+router.post('/register', function (req, res) {
   User.create({
     username: req.body.user.username,
     passwordhash: req.body.user.passwordhash,
   })
-    .then(function createSuccess(user) {
+    .then(function registerSuccess(user) {
       let token = jwt.sign(
         {
           id: user.id,
         },
-        'i_am_secret',
+        process.env.JWT_SECRET,
         { expiresIn: 60 * 60 * 60 }
       );
       res.json({
@@ -34,8 +34,13 @@ router.post('/login', function (req, res) {
   })
     .then(function loginSuccess(user) {
       if (user) {
+        let token = jwt.sign({ id: user.id }, process.env.JWT_SECRET, {
+          expiresIn: 60 * 60 * 24,
+        });
         res.status(200).json({
           user: user,
+          message: 'User successfully logged in!',
+          sessionToken: token,
         });
       } else {
         res.status(500).json({ error: 'User does not exist.' });
